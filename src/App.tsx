a6a37fa5279474ff01457357b3ea36fbd001ab33
Delfan <faceninja95@gmail.comm>
2025-02-15 13:29:22 +0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import CharacterList from './components/CharacterList';
+import CharacterDetail from './components/CharacterDetail';
+import './styles.css';
+
+const App: React.FC = () => {
+	return (
+		<Router>
+			<Routes>
+				<Route path='/' element={<CharacterList />} />
+				<Route path='/character/:id' element={<CharacterDetail />} />
+			</Routes>
+		</Router>
+	);
+};
+
+export default App;
